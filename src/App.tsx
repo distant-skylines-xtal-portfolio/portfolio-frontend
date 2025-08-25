d480fd3577e4befc9a205a3165cba04104b1fe7e
evan <212317146+Distant-Skyline-Xtal@users.noreply.github.com>
2025-08-25 17:41:31 +0900

    Add packages, remove unused files
    Added npm packages for react, typescript, testing and three JS.
    Removed Unnecessary files that are automatically added with package
    install i.e. react logo images.
    Removed boilerplate additions in App and index

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="Portfolio">
+      <header className="App-header">
+        <h1>Welcome to my portfolio!</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
