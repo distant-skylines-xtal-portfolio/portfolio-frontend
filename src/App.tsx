d4aea8bbb89ee3a827e19255713058496104b1c9
evan <212317146+Distant-Skyline-Xtal@users.noreply.github.com>
2025-08-27 12:49:53 +0900

    Add css reset, font and some basic styling
    Added CSS reset file, Saira variable font and some basic styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
-import './App.css';
+import MainCard from './components/MainCard';
 
 function App() {
   return (
-    <div className="Portfolio">
-      <header className="App-header">
-        <h1>Welcome to my portfolio!</h1>
-        <p>Adding some extra text to test pull requests</p>
-      </header>
+    <div className="app-body">
+      <MainCard />
     </div>
   );
 }
